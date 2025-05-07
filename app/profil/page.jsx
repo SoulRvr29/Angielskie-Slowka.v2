c73bfb9847c6199e8f66382cbc0ece597a0b6b90
Paweł Chudecki <pawelc29@interia.pl>
2025-05-07 12:34:48 +0200

    profile page

--- app/profil/page.jsx
+++ app/profil/page.jsx
@@ -1,4 +1,17 @@
+"use client";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+
 const ProfilPage = () => {
-  return <div>Mój profil</div>;
+  const { data: session } = useSession();
+  const profileImage = session?.user?.image;
+
+  return (
+    <div className="flex flex-col items-center gap-2 p-4 bg-base-300 w-fit mx-auto rounded-lg shadow-lg justify-center mt-8 max-sm:w-[90vw] font-semibold">
+      <Image src={profileImage} width={100} height={100} alt="profile image" />
+      <p>{session?.user.name}</p>
+      <p>{session?.user.email}</p>
+    </div>
+  );
 };
 export default ProfilPage;
