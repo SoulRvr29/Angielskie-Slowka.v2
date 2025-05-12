b55aad2705a65c95b073f1bd162af2482eb34ecf
Paweł Chudecki <pawelc29@interia.pl>
2025-05-12 10:40:40 +0200

    added suspense boundary

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,7 @@ import "@/assets/styles/spinner.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import AuthProvider from "./components/AuthProvider";
+import { Suspense } from "react";
 
 export const metadata = {
   title: "Angielskie-Słówka.v2",
@@ -11,19 +12,21 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <AuthProvider>
-      <html lang="en" data-theme="dark" className="bg-base-200">
-        <body className="relative max-w-6xl mx-auto flex flex-col bg-base-100">
-          <header className="sticky top-0 ">
-            <Navbar />
-          </header>
-          <main className="flex-grow flex flex-col px-4 py-2 max-sm:px-0">
-            {children}
-          </main>
-          <Footer />
-          <div className="absolute w-full h-full top-0 left-0 bg-black -z-10 blur-3xl opacity-30"></div>
-        </body>
-      </html>
-    </AuthProvider>
+    <Suspense>
+      <AuthProvider>
+        <html lang="en" data-theme="dark" className="bg-base-200">
+          <body className="relative max-w-6xl mx-auto flex flex-col bg-base-100">
+            <header className="sticky top-0 ">
+              <Navbar />
+            </header>
+            <main className="flex-grow flex flex-col px-4 py-2 max-sm:px-0">
+              {children}
+            </main>
+            <Footer />
+            <div className="absolute w-full h-full top-0 left-0 bg-black -z-10 blur-3xl opacity-30"></div>
+          </body>
+        </html>
+      </AuthProvider>
+    </Suspense>
   );
 }
